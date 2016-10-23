8bdc42077289ed7e8dfe126b76d10b89a0af5422
Claudio Bartoli <claudio@barto.li>
2016-10-23 18:15:20 +0200

    first boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	var base = [grunt.option('base') || '.', 'node_modules/reveal.js'];
+
+	// Project configuration
+	grunt.initConfig({
+
+		sass: {
+			themes: {
+				files: [
+					{
+						expand: true,
+						cwd: 'css/theme/source',
+						src: ['*.scss'],
+						dest: 'css/theme',
+						ext: '.css'
+					}
+				]
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: base,
+					livereload: true,
+					open: true
+				}
+			}
+		},
+
+		zip: {
+			'reveal-js-presentation.zip': [
+				'index.html',
+				'css/**',
+				'js/**',
+				'lib/**',
+				'images/**',
+				'plugin/**',
+				'**.md'
+			]
+		},
+
+		watch: {
+			js: {
+				files: [ 'Gruntfile.js', 'js/reveal.js' ],
+				tasks: 'js'
+			},
+			theme: {
+				files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+				tasks: 'css-themes'
+			},
+			css: {
+				files: [ 'css/reveal.scss' ],
+				tasks: 'css-core'
+			},
+			html: {
+				files: [ '*.html']
+			},
+			markdown: {
+				files: [ '*.md' ]
+			},
+			options: {
+				livereload: true
+			}
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+	grunt.loadNpmTasks( 'grunt-zip' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'css' ] );
+
+	// Theme CSS
+	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+
+	// All CSS
+	grunt.registerTask( 'css', [ 'css-themes' ] );
+
+	// Package presentation to archive
+	grunt.registerTask( 'package', [ 'default', 'zip' ] );
+
+	// Serve presentation locally
+	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+
+};
---
+++ slides/intro.md
@@ -0,0 +1,25 @@
+# Git init
+### for beginners 
+<!-- .element: class="fragment" data-fragment-index="1" -->
+
+---
+
+# we'll Cover
+> (A.K.A Agenda)
+
+- version controls
+- why git
+- git overview
+- git fundamentals
+- playng nice with other developers
+
+---
+
+# Disclaimer
+### for the next couple of hours
+
+- forget everything you may know about version control systems
+- forget everything you may know about git in particular
+- forget everything about you actual coding workflow
+
+> Start Fresh !!
